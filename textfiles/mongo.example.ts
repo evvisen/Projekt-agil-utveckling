92becff22996ec717112749738e75c6de18d5b6c
LACRIM94 <anasmestour@gmail.com>
2026-02-22 14:04:18 +0100

    Change mongodb connection from mongoose to mongodb

--- textfiles/mongo.example.ts
+++ textfiles/mongo.example.ts
@@ -1,10 +1,8 @@
-import mongoose from "mongoose";
-
-const MONGO_URI = "mongodb://127.0.0.1:dinport/quiz";
+import { MongoClient } from 'mongodb'
 
 export async function connectMongoDB(): Promise<void> {
 
-  await mongoose.connect(MONGO_URI);
+  new MongoClient('mongodb://localhost:dinport').db('quiz')
 
   console.log("Ansluten till MongoDB-databasen!");
 
