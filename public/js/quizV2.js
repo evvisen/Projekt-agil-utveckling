5c45d42399fe2b3ee412e7897d16f61c90a3a0be
LACRIM94 <anasmestour@gmail.com>
2026-02-26 14:38:35 +0100

    logik från labb

--- public/js/quizV2.js
+++ public/js/quizV2.js
@@ -21,3 +21,18 @@ async function test2(params) {
 };
 
 test2();
+
+/* fetch('https://api.spacexdata.com/v5/launches')
+    .then((response) => response.json())
+    .then((result) => {
+        console.log(result)
+
+        for (i = 0; i < 10; i++) {
+            let j = i + 1;
+            launchInfoContainers[j].children[0].innerHTML = result[i].name
+            launchInfoContainers[j].children[1].innerHTML = result[i].id
+            launchInfoContainers[j].children[2].innerHTML = result[i].flight_number
+            launchInfoContainers[j].children[3].innerHTML = result[i].date_local
+        }
+
+    }) */
