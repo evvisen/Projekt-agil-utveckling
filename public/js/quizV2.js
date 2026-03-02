f095aa73ed931f480b670ae524a6a589ea168fa7
evvisen <evelinahultquist@gmail.com>
2026-03-02 11:18:06 +0100

    deleted test functions

--- public/js/quizV2.js
+++ public/js/quizV2.js
@@ -1,4 +1,4 @@
-const answers = document.querySelector("#answers")
+const answers = document.querySelector("#answers");
 
 async function svarsalternativ(params) {
   await fetch("http://localhost:3000/api/juridikquiz")
@@ -29,44 +29,6 @@ async function svarsalternativ(params) {
 
 svarsalternativ();
 
-async function test(params) {
-  await fetch("http://localhost:3000/api/ekonomiquiz")
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-      console.log(result[0].svarsalternativ1[0]);
-    });
-}
-
-test();
-
-async function test2(params) {
-  await fetch("http://localhost:3000/api/questions")
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-      console.log(result.questions[5].correct_option);
-    });
-}
-
-test2();
-
-/* fetch('https://api.spacexdata.com/v5/launches')
-    .then((response) => response.json())
-    .then((result) => {
-        console.log(result)
-
-        for (i = 0; i < 10; i++) {
-            let j = i + 1;
-            launchInfoContainers[j].children[0].innerHTML = result[i].name
-            launchInfoContainers[j].children[1].innerHTML = result[i].id
-            launchInfoContainers[j].children[2].innerHTML = result[i].flight_number
-            launchInfoContainers[j].children[3].innerHTML = result[i].date_local
-        }
-
-    })*/
-
-//Evelina :)
 async function getQuestions() {
   const response = await fetch("http://localhost:3000/api/questions");
   const result = await response.json();
